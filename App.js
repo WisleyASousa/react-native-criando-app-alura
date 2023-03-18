451fb3857b1e01f098ff6079a3ea81b59ebd2e9c
WisleyASousa <wisley.sousa@outlook.com.br>
2023-03-18 11:19:33 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StatusBar } from 'react-native';
+import Cesta from './src/screens/Cesta';
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+import { View } from 'react-native-web';
+
 
 export default function App() {
+
+  // Carregando as fontes instaladas com o expo-font
+  const [fontCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+  });
+
+  // Enquanto as fontes não forem carregadas, não renderiza o componente, vai aparecer uma tela em branco
+  if (!fontCarregada) {
+    return <View />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <SafeAreaView>
+        <StatusBar />
+        <Cesta />
+      </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
